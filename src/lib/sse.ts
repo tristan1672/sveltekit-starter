115a8696595e4c37c55629f233ca22138e7c18fb
tristan1672 <115226386+tristan1672@users.noreply.github.com>
2025-06-03 14:11:45 +0800

    optional route without unique name

--- src/lib/sse.ts
+++ src/lib/sse.ts
@@ -20,3 +20,21 @@ export function initSSE(onMessage: (data: any) => void) {
 
   return eventSource;
 }
+
+export function init_SSE(onMessage: (data: any) => void) {
+  const eventSource = new EventSource(`${edgeDeviceConfig.backendUrl}/event`);
+
+  eventSource.onopen = () => console.log("SSE connection opened");
+  eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log("[SSE] Received message:", data);
+    onMessage(data);
+  };
+
+  eventSource.onerror = (err) => {
+    console.error("SSE error:", err);
+    eventSource.close();
+  };
+
+  return eventSource;
+}
